eabfc1c5c927f0d508bdc0b5fc90ed4347b3b303
Moe Kurdi <kurdi1128@gmail.com>
2021-11-28 03:12:16 -0500

    added basic eventlstnrs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './scss/main.scss';
+
+const _todoInput = document.querySelector('.todo-input');
+const _todoButton = document.querySelector('.todo-button');
+const _todoList = document.querySelector('.todo-list');
+
+
+
+document.addEventListener('DOMContentLoaded', getTodo);
+_todoList.addEventListener('click', delTodo);
+_todoButton.addEventListener('click', addTodo);
+
